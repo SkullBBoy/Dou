e4527422393170c351928acec5b063b057d6488f
Oliva <roman57izqui@hotmail.com>
2023-10-27 14:02:13 -0300

    Juego progreso

--- JS/juego.js
+++ JS/juego.js
@@ -1,6 +1,41 @@
-let volver = document.getElementById("firstItem");
-let volver2= document.getElementById("miditem");
-let saldox= document.getElementById("saldo");
-volver.setAttribute("href", `cuartojuegos.html `);
+const botonIniciar = document.getElementById("botonIniciar");
+const elementosHoverA = document.querySelectorAll(".hoverA");
+let audio = document.getElementById('audioElement');
+let cuadradoJuego = document.getElementById("objetoClick");
+
+botonIniciar.addEventListener('click', () => {
+  document.body.style.backgroundImage = `url("../IMG/fondojuego.jpg")`;
+  botonIniciar.style.display = "none";
+  cuadradoJuego.style.display = "block";
+  elementosHoverA.forEach(elemento => {               //
+    elemento.addEventListener("mouseenter", () => {   // Cambiar el Hover
+      elemento.style.backgroundColor = "black";
+      elemento.style.color = "white";
+    });
+
+    elemento.addEventListener("mouseleave", () => {
+      elemento.style.backgroundColor = "";
+      elemento.style.color = "white";
+    });
+  });                                                // Fin cambiar el hover
+  audio.src = "../Audio/Musicajuego.mp3"; 
+  audio.play(); 
+  audio.addEventListener("ended", function() {
+    audio.currentTime = 0; 
+    audio.play(); 
+  });
+
+  setInterval(Juego, 800);
 
 
+  
+});
+
+function Juego(){
+    let topRandom = Math.floor(Math.random() * (482 - 127 + 1)) + 127;
+    let leftRandom = Math.floor(Math.random() * (762 - 77 + 1)) + 77;
+    cuadradoJuego.style.top = `${topRandom}px`;
+    cuadradoJuego.style.left = `${leftRandom}px`;
+    console.log(cuadradoJuego.style.top);
+  
+}
--- JS/random.js
+++ JS/random.js
@@ -2,18 +2,10 @@ const boton = document.getElementById("lastItem2");
 const boton2= document.getElementById("firstItem2");
 let audio = document.getElementById('audioElement');
 let ultimoSonidoReproducido = localStorage.getItem('ultimoSonidoReproducido');
+let verificar
 
-window.addEventListener("keydown", (event) => {
-  if (event.key == "ArrowRight") {
-    window.location.href = "cuartojuegos.html";
-  }
-  if (event.key == "ArrowLeft") {
-    window.location.href = "cocina.html"
-  }
-});
-
-
-boton.addEventListener('click', function() {
+function Sonar(){
+  
   let numeroAleatorio = Math.floor(Math.random() * 35) + 1; 
   let nombreSonido = "../Audio/" + numeroAleatorio + '.mp3';
   
@@ -22,7 +14,27 @@ boton.addEventListener('click', function() {
   
   ultimoSonidoReproducido = nombreSonido;
   localStorage.setItem('ultimoSonidoReproducido', ultimoSonidoReproducido);
+
+}
+
+window.addEventListener("keydown", (event) => {
+  if (event.key == "ArrowRight") {
+    window.location.href = "cuartojuegos.html";
+  }
+  if (event.key == "ArrowLeft") {
+    window.location.href = "cocina.html"
+  }
+  if(event.key=="Enter"){
+    Sonar()
+   
+  }
+
 });
+
+
+
+
+boton.addEventListener('click', Sonar() );
 
 
 boton2.addEventListener('click', function() {
