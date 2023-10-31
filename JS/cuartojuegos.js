e59262164493c440d9ce99f50589740b184a9a82
Oliva <roman57izqui@hotmail.com>
2023-10-31 05:42:35 -0300

    En progreso

--- JS/cuartojuegos.js
+++ JS/cuartojuegos.js
@@ -3,45 +3,64 @@ const boton = document.getElementById("lastItem2");
 const boton2 = document.getElementById("miditem2")
 const boton3 = document.getElementById("firstItem2")
 const pelota = document.getElementById("pelota")
+//              DOU SKIN ---------------------------------------------------
+let Saldo =  JSON.parse(localStorage.getItem("Saldo"));
 let Compras = localStorage.getItem("Compras")
 let Dou = document.getElementById("Dou")
 let carrito=[]
 let pelo= document.getElementById("pelo")
 
-if(Compras.includes("milei")){ //              DOU SKIN ----------------------------------
-  pelo.src="../IMG/peluca.png"
-  pelo.style.position="absolute"
-  pelo.style.width="180px"
-  pelo.style.top="174px"
-  pelo.style.left="557px"
-  pelo.style.zIndex="2"
-  }
-  else if(Compras.includes("goku")){
-    pelo.src="../IMG/goku.png"
+if (Compras !== null) {
+  if(Compras.includes("milei")){ 
+    pelo.src="./IMG/peluca.png"
     pelo.style.position="absolute"
-    pelo.style.width="267px"
-    pelo.style.top="111px"
-    pelo.style.left="507px"
+    pelo.style.width="180px"
+    pelo.style.top="174px"
+    pelo.style.left="545px"
     pelo.style.zIndex="2"
-   
-  }
-  if(Compras.includes("caraAleatoria")){
-  let num=localStorage.getItem("Skin")
-  Dou.src="../IMG/skins/"+num+".png";
-  }
-  if (Compras.includes("fondosAleatorios")) {
-    let fondo = Math.floor(Math.random() * 16) + 1;
-    let rutaFondo = "../IMG/fondos/" + fondo + ".jpg";
-    document.body.style.backgroundImage = "url('" + rutaFondo + "')";
-  }
-  else{
-    document.body.style.backgroundImage = "url('../IMG/fondoazul.jpg')";
-    
-  } 
-  if (Dou.getAttribute("src") === "") {
-    Dou.src = "../IMG/skinbase.png";
-  }
-  // ------------------------------------------------------------------- JS Dou
+    }
+    else if(Compras.includes("goku")){
+      pelo.src="./IMG/goku.png"
+      pelo.style.position="absolute"
+      pelo.style.width="267px"
+      pelo.style.top="111px"
+      pelo.style.left="495px"
+      pelo.style.zIndex="2"
+     
+    }
+    if(Compras.includes("caraAleatoria")){
+    let num=localStorage.getItem("Skin")
+    Dou.src="./IMG/skins/"+num+".png";
+    }
+    if (Compras.includes("fondosAleatorios")) {
+      let fondo = Math.floor(Math.random() * 16) + 1;
+      let rutaFondo = "./IMG/fondos/" + fondo + ".jpg";
+      document.body.style.backgroundImage = "url('" + rutaFondo + "')";
+    }
+    else{
+      document.body.style.backgroundImage = "url('../IMG/fondoazul.jpg')";
+      
+    } 
+    if(Dou.src==""){
+      Dou.src="./IMG/skinbase.png"
+      
+    }
+ } else {
+  localStorage.setItem("Compras", JSON.stringify(carrito));
+}
+
+/* JSON.parse() */
+
+
+
+if (Saldo !== null) {
+} else {
+  Saldo=0
+  localStorage.setItem("Saldo",Saldo);
+}
+
+// ------------------------------------------------------------------- JS Dou
+
 
  
 window.addEventListener("keydown", (event) => {
--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,8 @@ const linterna= document.getElementById("miditem2");
 const overlay= document.getElementById("overlay")
 const linternafoto = document.getElementById("linternafoto")
 const prendido = localStorage.getItem("prendido")
+
+//              DOU SKIN ---------------------------------------------------
 let Saldo =  JSON.parse(localStorage.getItem("Saldo"));
 let Compras = localStorage.getItem("Compras")
 let Dou = document.getElementById("Dou")
@@ -12,51 +14,45 @@ let carrito=[]
 let pelo= document.getElementById("pelo")
 
 if (Compras !== null) {
+  if(Compras.includes("milei")){ 
+    pelo.src="./IMG/peluca.png"
+    pelo.style.position="absolute"
+    pelo.style.width="180px"
+    pelo.style.top="174px"
+    pelo.style.left="545px"
+    pelo.style.zIndex="2"
+    }
+    else if(Compras.includes("goku")){
+      pelo.src="./IMG/goku.png"
+      pelo.style.position="absolute"
+      pelo.style.width="267px"
+      pelo.style.top="111px"
+      pelo.style.left="495px"
+      pelo.style.zIndex="2"
+     
+    }
+    if(Compras.includes("caraAleatoria")){
+    let num=localStorage.getItem("Skin")
+    Dou.src="./IMG/skins/"+num+".png";
+    }
+    if (Compras.includes("fondosAleatorios")) {
+      let fondo = Math.floor(Math.random() * 16) + 1;
+      let rutaFondo = "./IMG/fondos/" + fondo + ".jpg";
+      document.body.style.backgroundImage = "url('" + rutaFondo + "')";
+    }
+    else{
+      document.body.style.backgroundImage = "url('../IMG/fondoazul.jpg')";
+      
+    } 
+    if(Dou.src==""){
+      Dou.src="./IMG/skinbase.png"
+      
+    }
  } else {
   localStorage.setItem("Compras", JSON.stringify(carrito));
 }
 
 /* JSON.parse() */
-
-if(Compras.includes("milei")){ //              DOU SKIN ----------------------------------
-pelo.src="./IMG/peluca.png"
-pelo.style.position="absolute"
-pelo.style.width="180px"
-pelo.style.top="174px"
-pelo.style.left="545px"
-pelo.style.zIndex="2"
-}
-else if(Compras.includes("goku")){
-  pelo.src="./IMG/goku.png"
-  pelo.style.position="absolute"
-  pelo.style.width="267px"
-  pelo.style.top="111px"
-  pelo.style.left="495px"
-  pelo.style.zIndex="2"
- 
-}
-if(Compras.includes("caraAleatoria")){
-let num=localStorage.getItem("Skin")
-Dou.src="./IMG/skins/"+num+".png";
-}
-if (Compras.includes("fondosAleatorios")) {
-  let fondo = Math.floor(Math.random() * 16) + 1;
-  let rutaFondo = "./IMG/fondos/" + fondo + ".jpg";
-  document.body.style.backgroundImage = "url('" + rutaFondo + "')";
-}
-else{
-  document.body.style.backgroundImage = "url('../IMG/fondoazul.jpg')";
-  
-} 
-if(Dou.src==""){
-  Dou.src="./IMG/skinbase.png"
-}
-
-
-// ------------------------------------------------------------------- JS Dou
-
-
-
 
 
 
@@ -65,6 +61,14 @@ if (Saldo !== null) {
   Saldo=0
   localStorage.setItem("Saldo",Saldo);
 }
+
+// ------------------------------------------------------------------- JS Dou
+
+
+
+
+
+
 
 window.addEventListener("keydown", (event) => {
   if (event.key == "ArrowRight") {
